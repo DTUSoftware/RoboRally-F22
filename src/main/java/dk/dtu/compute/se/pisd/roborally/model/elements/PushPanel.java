db8d0b38513f79f6abdc7d8a1c53c1ea69c01f0a
Zetles96 <90190656+Zetles96@users.noreply.github.com>
2022-04-30 16:39:50 +0200

    need to pull

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/elements/PushPanel.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/elements/PushPanel.java
@@ -3,8 +3,10 @@ package dk.dtu.compute.se.pisd.roborally.model.elements;
 import dk.dtu.compute.se.pisd.roborally.controller.GameController;
 import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
-public class PushPanel extends ActionElement{
+
+public class PushPanel extends ActionElement {
 // TODO make this stuff
+
     /**
      * Constructer for action element
      *
@@ -14,7 +16,8 @@ public class PushPanel extends ActionElement{
     public PushPanel(GameController gameController, Space space, Heading direction) {
 
         super(gameController, space);
-        Wall wallOn = new Wall(space,direction);
+        
+        Wall wallOn = new Wall(space, direction);
     }
 
     @Override
